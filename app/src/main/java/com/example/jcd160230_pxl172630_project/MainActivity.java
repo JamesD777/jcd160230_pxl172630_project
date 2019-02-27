be9acf4f4b8e2a58884acc33903c24d0a37eab0d
James Dunlap <masterjd777@hotmail.com>
2019-02-26 23:26:02 -0600

    Start Files

---
+++ app/src/main/java/com/example/jcd160230_pxl172630_project/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.jcd160230_pxl172630_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Fragment f = new Fragment();
+        //FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        //        fragmentTransaction.add(R.id.container, this.f);
+        //        fragmentTransaction.commit();
+    }
+}
