30c0dd54b9f87e7c6877cd0e4df0007a9f36002b
unknown <pear.was@gmail.com>
2019-03-04 20:58:49 -0600

    final comments

--- app/src/main/java/com/example/jcd160230_pxl172630_project/File_IO.java
+++ app/src/main/java/com/example/jcd160230_pxl172630_project/File_IO.java
@@ -1,3 +1,13 @@
+/******************************************************************************
+ * This is an application written for 4301.002, to display a contact list in an
+ * android app that is modifiable by the user. It has a list that opens up a
+ * specific contact's info when you click their name. This contact information
+ * can be modified by the user and is saved when the save button is clicked.
+ *
+ * Written by James Dunlap(jcd160230) and Perry Lee (pxl172630) at The University
+ * of Texas at Dallas starting March 4, 2019, for an Android development course.
+ ******************************************************************************/
+
 package com.example.jcd160230_pxl172630_project;
 
 import android.content.Context;
