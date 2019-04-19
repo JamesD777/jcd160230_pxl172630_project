d5733c1d0e0fa9c710e96b2b2fc594dfabe45347
JamesD777 <jcd160230@utdallas.edu>
2019-04-19 18:07:56 -0500

    added maps and merged perrys shit

--- app/src/main/java/com/example/jcd160230_pxl172630_project/MapActivity.java
+++ app/src/main/java/com/example/jcd160230_pxl172630_project/MapActivity.java
@@ -1,7 +1,11 @@
 package com.example.jcd160230_pxl172630_project;
 
-import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
 
 public class MapActivity extends AppCompatActivity {
 
@@ -11,5 +15,18 @@ public class MapActivity extends AppCompatActivity {
         setContentView(R.layout.activity_map);
 
         String receivedAddress = getIntent().getStringExtra("contactAddress");
+        addFragment(new MapFragment(), false, "one");
+
+    }
+
+    public void addFragment(Fragment fragment, boolean addToBackStack, String tag) {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction ft = manager.beginTransaction();
+
+        if (addToBackStack) {
+            ft.addToBackStack(tag);
+        }
+        ft.replace(R.id.container, fragment, tag);
+        ft.commitAllowingStateLoss();
     }
 }
