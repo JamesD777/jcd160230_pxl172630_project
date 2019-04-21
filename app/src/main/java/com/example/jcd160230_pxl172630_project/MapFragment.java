eb6ebe8507ccb063fcd76ba5a93a170ad80158b8
unknown <pear.was@gmail.com>
2019-04-21 17:17:16 -0500

    latlng

--- app/src/main/java/com/example/jcd160230_pxl172630_project/MapFragment.java
+++ app/src/main/java/com/example/jcd160230_pxl172630_project/MapFragment.java
@@ -100,7 +100,17 @@ public class MapFragment extends Fragment {
                     // for ActivityCompat#requestPermissions for more details.
                     return;
                 }
-                mMap.setMyLocationEnabled(true);
+
+                LatLng sydney = new LatLng(-33.867, 151.206);
+
+                map.setMyLocationEnabled(true);
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
+
+                map.addMarker(new MarkerOptions()
+                        .title("Sydney")
+                        .snippet("The most populous city in Australia.")
+                        .position(sydney));
+
             }
         });
 
