dab6365d4b588ebaa5a36480ca06458037045e05
JamesD777 <jcd160230@utdallas.edu>
2019-04-22 22:51:58 -0500

    Added a fix for the map to properly move to the correct location

--- app/src/main/java/com/example/jcd160230_pxl172630_project/MapFragment.java
+++ app/src/main/java/com/example/jcd160230_pxl172630_project/MapFragment.java
@@ -6,22 +6,16 @@ import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
-import android.location.Address;
-import android.location.Criteria;
-import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -34,13 +28,9 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 
-import java.util.List;
-import java.util.concurrent.Executor;
 
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements OnMapReadyCallback{
 
     public MapFragment() {
         // Required empty public constructor
@@ -53,65 +43,50 @@ public class MapFragment extends Fragment {
     }
 
     static GoogleMap map;
-    private static String distance;
-    Geocoder geocoder;
-    String bestProvider;
-    List<Address> user = null;
     static double currentLat;
     static double currentLng;
-    private FusedLocationProviderClient mFusedLocationProviderClient;
-    private Location mLastKnownLocation;
+
+    private FusedLocationProviderClient fusedLocationClient;
+
+    private LocationListener locationListener;
+    private LocationManager locationManager;
+    public static Location location;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_map, container, false);
-
-        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getContext());
-
-//        Criteria criteria = new Criteria();
-//        bestProvider = lm.getBestProvider(criteria, false);
-//        if (ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {}
-//        Location location = lm.getLastKnownLocation(bestProvider);
-
-//        if (location == null){
-//            Toast.makeText(this.getActivity(),"Location Not found",Toast.LENGTH_LONG).show();
-//        }else{
-//            geocoder = new Geocoder(this.getActivity());
-//            try {
-//                user = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
-//                currentLat=(double)user.get(0).getLatitude();
-//                currentLng=(double)user.get(0).getLongitude();
-//                System.out.println(" DDD lat: " +currentLat+",  longitude: "+currentLng);
-//
-//            }catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//        }
-
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(getContext());
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.frg);  //use SuppoprtMapFragment for using in fragment instead of activity  MapFragment = activity   SupportMapFragment = fragment
-        mapFragment.getMapAsync(new OnMapReadyCallback() {
+        mapFragment.getMapAsync(this);
+
+
+
+        locationListener = new LocationListener() {
             @Override
-            public void onMapReady(GoogleMap mMap) {
-                map = mMap;
-                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-
-                mMap.clear(); //clear old markers
-
-                getDeviceLocation();
-
-                CameraPosition currentLocation = CameraPosition.builder()
-                        .target(new LatLng(34,96))
-                        .zoom(10)
-                        .bearing(0)
-                        .tilt(45)
-                        .build();
-
-                map.animateCamera(CameraUpdateFactory.newCameraPosition(currentLocation), 10000, null);
+            public void onLocationChanged(Location loc) {
+                location = loc;
+                System.out.println("curr loc: " + location);
             }
-        });
 
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        locationManager = (LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE);
 
         return rootView;
     }
@@ -126,43 +101,37 @@ public class MapFragment extends Fragment {
     }
 
     public static void updateMap(double lat, double lng){
+        if (location != null) {
+            LatLng marker = new LatLng(lat, lng);
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(marker, 15));
+        }
+
+        map.addMarker(new MarkerOptions()
+                .position(new LatLng(lat,lng))
+                .title("Address"));
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        map.clear(); //clear old markers
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        this.fusedLocationClient.getLastLocation();
+        currentLat = location.getLatitude();
+        currentLng = location.getLongitude();
+
         CameraPosition currentLocation = CameraPosition.builder()
-                .target(new LatLng(lat,lng))
+                .target(new LatLng(currentLat,currentLng))
                 .zoom(10)
                 .bearing(0)
                 .tilt(45)
                 .build();
 
         map.animateCamera(CameraUpdateFactory.newCameraPosition(currentLocation), 10000, null);
-
-        map.addMarker(new MarkerOptions()
-                .position(new LatLng(lat,lng))
-                .title("Address")
-                .snippet(distance));
-
-    }
-
-    private void getDeviceLocation() {
-        /*
-         * Get the best and most recent location of the device, which may be null in rare
-         * cases when a location is not available.
-         */
-        try {
-            Task<Location> locationResult = mFusedLocationProviderClient.getLastLocation();
-            locationResult.addOnCompleteListener( this.getActivity(), new OnCompleteListener<Location>() {
-                @Override
-                public void onComplete(@NonNull Task<Location> task) {
-                    if (task.isSuccessful()) {
-                        // Set the map's camera position to the current location of the device.
-                        mLastKnownLocation = task.getResult();
-                        map.moveCamera(CameraUpdateFactory.newLatLngZoom(
-                                new LatLng(mLastKnownLocation.getLatitude(),
-                                        mLastKnownLocation.getLongitude()), 15));
-                    }
-                }
-            });
-        } catch (SecurityException e)  {
-            Log.e("Exception: %s", e.getMessage());
-        }
     }
 }
